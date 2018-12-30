bb03d1d5d74e8ab9a4898ae9a0fcbaa7d6234e98
Robert Ying <yingrui205@gmail.com>
2018-12-31 01:57:34 +0800

    Use autotools

---
+++ autogen.sh
@@ -0,0 +1,10 @@
+cp cJSON/cJSON.c lib/
+cp cJSON/cJSON.h lib/
+cp sds/sds.c lib/
+cp sds/sds.h lib/
+cp sds/sdsalloc.h lib/
+cp utf8proc/utf8proc.c lib/
+cp utf8proc/utf8proc.h lib/
+cp utf8proc/utf8proc_data.c lib/
+
+autoreconf --install || exit 1
