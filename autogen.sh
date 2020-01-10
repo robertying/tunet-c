0704824880823be4960b15d9c6f8121eddf113da
Rui Ying <yingrui205@gmail.com>
2020-01-10 18:43:08 +0800

    Use pre-installed libs & drop Windows support

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-cd openssl
-./config
-make
-cd ..
-
 mkdir -p m4
-cp cJSON/cJSON.c lib/
-cp cJSON/cJSON.h lib/
-cp sds/sds.c lib/
-cp sds/sds.h lib/
-cp sds/sdsalloc.h lib/
-cp utf8proc/utf8proc.c lib/
-cp utf8proc/utf8proc.h lib/
-cp utf8proc/utf8proc_data.c lib/
-
 autoreconf --install || exit 1
